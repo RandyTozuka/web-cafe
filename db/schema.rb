1cd01818c4d1a0a346ba6614102cbf99f6e8a00a
Randy Tozuka <kazm1208@me.com>
2020-10-13 07:17:32 +0900

    13th Oct 2020

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_24_215937) do
+ActiveRecord::Schema.define(version: 2020_10_11_215516) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2020_09_24_215937) do
     t.string "provider"
     t.string "uid"
     t.string "username"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
